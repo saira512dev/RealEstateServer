61ce5da6bbf7ee256833ad6a85dc5a385d1b802f
saira512dev <512saira@gmail.com>
2025-05-14 12:00:00 +0000

    Added routes for tenants and managers

--- dist/src/index.js
+++ dist/src/index.js
@@ -9,8 +9,11 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
+const authMiddleware_1 = require("./middleware/authMiddleware");
 // ROUTE IMPORT
 console.log('🚀  Starting Express app from src/index.ts');
+const tenantRoutes_1 = __importDefault(require("./routes/tenantRoutes"));
+const managerRoutes_1 = __importDefault(require("./routes/managerRoutes"));
 // CONFIG
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -26,6 +29,8 @@ app.get('/', (req, res) => {
     console.log("🔥 HIT /");
     res.send("this is home page");
 });
+app.use("/tenants", (0, authMiddleware_1.authMiddleware)(["tenant"]), tenantRoutes_1.default);
+app.use("/managers", (0, authMiddleware_1.authMiddleware)(["manager"]), managerRoutes_1.default);
 // SERVER
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
